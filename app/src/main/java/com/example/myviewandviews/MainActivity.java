78f7bdff76d1dfda5c313a9bf4c351547aac0343
ACER <melanisafwa1304@gmail.com>
2022-12-02 00:58:12 +0700

    commit

--- app/src/main/java/com/example/myviewandviews/MainActivity.java
+++ app/src/main/java/com/example/myviewandviews/MainActivity.java
@@ -1,17 +1,55 @@
 package com.example.myviewandviews;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Thread view;
+    private TextView tvNama, tvHarga;
+    String nama, deskripsi, harga, jumlah;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (getSupportActionBar() !=null){
-            getSupportActionBar().setTitle("Google Pixel");
+        tvNama = findViewById(R.id.tv_nama);
+        tvHarga = findViewById(R.id.tv_harga);
+
+        nama = tvNama.getText().toString();
+        harga = tvHarga.getText().toString();
+
+        Button btnBeli = findViewById(R.id.btn_beli);
+        btnBeli.setOnClickListener(this);
+
+        Button btnDialPhone = findViewById(R.id.btn_cp);
+        btnDialPhone.setOnClickListener(this);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(nama);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() ==  R.id.btn_beli){
+            Intent moveIntent = new Intent(MainActivity.this, BeliActivity.class);
+            moveIntent.putExtra(BeliActivity.EXTRA_NAMA, nama);
+            moveIntent.putExtra(BeliActivity.EXTRA_HARGA, harga);
+            startActivity(moveIntent);
+        }
+        else if (view.getId()== R.id.btn_cp){
+            String phoneNumber = "0859155035552";
+            Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+            startActivity(dialPhoneIntent);
         }
     }
 }
